c0cbe66230d151be1d8b71ea9f9d30a5236c7118
Makhi7 <mvmangxola22@gmail.com>
2023-05-05 08:20:42 +0200

    finished About component, starting with Skills component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const About = () =>{
     return (
@@ -6,11 +7,39 @@ const About = () =>{
             <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
                 <div className='col-span-2'>
                     <p>About</p>
+
                     <h2>Who I am</h2>
-                    <p></p>
+
+                    <p>Lorem ipsum dolor sit.</p>
+
+                    <p>Lorem ipsum dolor, sit amet 
+                        consectetur adipisicing elit. Accusantium harum fuga 
+                        fugiat laborum officia vero incidunt in, omnis 
+                        corrupti ut aspernatur consequuntur hic ratione ab 
+                        deleniti deserunt.
+                    </p>
+
+                    <p>Lorem ipsum dolor, sit amet consectetur 
+                        adipisicing elit. Voluptate nam provident 
+                        nesciunt ipsam inventore consequatur quis 
+                        saepe, at labore expedita ducimus aut 
+                        necessitatibus non nulla atque repellendus? 
+                        Dolores, at maxime.
+                    </p>
+
+                    <p className='py-2 text-gray-600 underline cursor-pointer'>Check some of my latest projects</p>
+
+                </div>
+                <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+                    <Image src='/../public/assets/makhi-mangxola.png' 
+                    alt='' 
+                    width='1000' 
+                    height='1500'/>
                 </div>
 
             </div>
         </div>
     );
 }
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import Main from '@/components/Main';
-// import About from '@/components/About';
+import About from '@/components/About';
 
 
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
       <Navbar />
       <Main />
-      {/* <About /> */}
+      <About />
 
     
     </div>
